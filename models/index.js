863f4dbefa30300b1e3cb9cfb38d2d4c2875c1ea
Brian <bmccann36@gmail.com>
2018-09-16 16:50:51 -0400

    forgot to commit change to index.js see prev commit

--- models/index.js
+++ models/index.js
@@ -6,15 +6,28 @@ const Tech = require('./tech');
 
 // PROJECT ASSOCIATION WITH CLIENTS
 Project.belongsTo(Client);
-Client.hasMany(Project);
+Client.hasMany(Project, {
+  onDelete: 'cascade',
+  hooks: true,
+});
 
 //  EMPLOYEE ASSOSIATION TO PROJECT
-Employee.belongsToMany(Project, { through: 'employee_project' });
-Project.belongsToMany(Employee, { through: 'employee_project' });
+Employee.belongsToMany(Project, {
+  through: 'employee_project',
+});
+Project.belongsToMany(Employee, {
+  through: 'employee_project',
+  // onDelete: 'cascade',
+  // hooks: true,
+});
 
 // TECHNOLOGY ASSOCIATION TO EMPLOYEE
 Tech.belongsToMany(Employee, { through: 'technology_employee' })
 Employee.belongsToMany(Tech, { through: 'technology_employee' })
+
+
+
+
 
 module.exports = {
   Project,
